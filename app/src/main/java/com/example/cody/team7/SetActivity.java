8f555202d21957f17ae8d86cd95fe1687c38ed99
Cody <cody.s.snyder@gmail.com>
2016-10-26 17:54:35 -0400

    Meeting2 Commit

--- app/src/main/java/com/example/cody/team7/SetActivity.java
+++ app/src/main/java/com/example/cody/team7/SetActivity.java
@@ -1,13 +1,34 @@
 package com.example.cody.team7;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SetActivity extends AppCompatActivity {
+public class SetActivity extends AppCompatActivity implements View.OnClickListener{
+    Button submitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.submitButton:
+                //do whatever
+                break;
+        }
     }
 }
